232ce0826420a6030dc60b4198a137c1394ba593
Steven Zapata <szapata427@gmail.com>
2018-09-01 20:22:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(numbers)
+  
+  if 2 > numbers
+    return false
+  end
+  
+(2...numbers). each do |factor|
+  if numbers % factor == 0
+  return false
+  end
+  
+end
+return true
+end
