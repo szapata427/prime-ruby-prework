fef95045d34e48c699026e152e356fe586f490f7
Steven Zapata <szapata427@gmail.com>
2018-09-01 20:10:30 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,11 @@
-# Add  code here!
 def prime?(numbers)
   
-  numbers = (1..numbers)
+  if 2 > numbers
+    return false
   
-  numbers.each do |number|
-    if number % 
+(2..numbers). each do |factor|
+  
+  elsif numbers % factor == 0
+  return true
+  end
+end
